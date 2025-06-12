166f67f5c98f4f7df9efb7c45faf643aa088aad2
remimimimimi <remimimimimi@protonmail.com>
2025-06-12 16:04:33 +0300

    Configure proof general

--- init.el
+++ init.el
@@ -588,7 +588,6 @@ If ARG ≥ 16, prompt for both TITLE and TAGS."
 
 (use-package lean4-mode
   :ensure (:type git :host github :repo "bustercopley/lean4-mode" :branch "eglot" :files ("*.el" "data"))
-
   :custom (lean4-keybinding-refresh-file-dependencies (kbd "C-c d")))
 
 (use-package paredit :ensure t
@@ -606,6 +605,12 @@ If ARG ≥ 16, prompt for both TITLE and TAGS."
 
 (use-package glsl-mode
   :ensure t)
+
+(use-package proof-general
+  :ensure t
+  :config
+  (push '(narya "Narya" "ny" nil (".nyo")) proof-assistant-table)
+  (push 'narya proof-general-configured-provers))
 
 ;;; Custom functions
 (defun sudo-find-file (file-name)
